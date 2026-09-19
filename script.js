4ec387e50ae5546a983635d4f0bed9269cde3b88
salah-2018 <43065744+salah-2018@users.noreply.github.com>
2026-09-19 13:01:32 +0100

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var PRODUCTS_URL = './products.json';
-  var GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyWarAGLl0uCB2heO41fvHJ7ZImSo2sEThJq2jDta8A8I9yzISzY6GFu0wck-ZQg4qNmA/exec';
+  var GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzGHtxiDbY4RR3Ylr3m42zS1JwyhaZgn3Tzfocnk96CJKnIXhb59HwO-RfS2eH5faoZig/exec';
 
   var GALLERY_IMAGES_COUNT = 2; 
 
